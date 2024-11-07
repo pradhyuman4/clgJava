4538bfbf1dff15dd13553b97ba746ccc342a1a7e
pradhyuman4 <patelpradhyuman4444@gmail.com>
2024-11-07 18:40:39 +0530

    Add files via upload

---
+++ assg1_que5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class assg1_que5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        System.out.println(a + " = "+ a*1.609 + " miles ");
+    }
+}
