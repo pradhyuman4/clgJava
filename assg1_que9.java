ef5c35941d461ceed730aff67d6a63ffa0279245
pradhyuman4 <patelpradhyuman4444@gmail.com>
2024-11-07 19:03:47 +0530

    Add files via upload

---
+++ assg1_que9.java
@@ -0,0 +1,10 @@
+public class assg1_que9 {
+    public static void main(String[] args) {
+        long ans = 1;
+        for(int i = 12; i>0; i--){
+            ans = ans*i;
+
+        }
+        System.out.println("factorial of 12 is " + ans);
+    }
+}
