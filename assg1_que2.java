ff8151ffb5585641b3ece07116d42a30e0277b08
pradhyuman4 <patelpradhyuman4444@gmail.com>
2024-11-07 18:24:15 +0530

    Add files via upload

---
+++ assg1_que2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class assg1_que2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num>0){
+            System.out.println("number is positive");
+        }
+        else{
+            System.out.println("number is negative");
+        }
+
+    }
+}
