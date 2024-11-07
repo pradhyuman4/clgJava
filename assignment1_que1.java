97a65b1a6c56fafa2a4fbcff5464c698bbe3ab67
pradhyuman4 <patelpradhyuman4444@gmail.com>
2024-11-07 18:14:54 +0530

    Add files via upload

---
+++ assignment1_que1.java
@@ -0,0 +1,10 @@
+public class assignment1_que1 {
+    public static void main(String[] args) {
+        float tempindegree = 33;
+        //to change in fahreneit we to multiply it by 33.8
+
+        System.out.println((tempindegree*9/5)+32);
+        //and to change fareneit to degree celcius
+        System.out.println((tempindegree-32)*5/9);
+    }
+}
