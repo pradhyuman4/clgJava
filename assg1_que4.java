db22b36ba533990dc6eeff832ec4622bbf24b13b
pradhyuman4 <patelpradhyuman4444@gmail.com>
2024-11-07 18:37:14 +0530

    Add files via upload

---
+++ assg1_que4.java
@@ -0,0 +1,11 @@
+public class assg1_que4 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+        int temp = b;
+        b = a;
+        a = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
