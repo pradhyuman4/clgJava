6f28059515dc45bdbf89b9ed99103bfc6c32ad93
pradhyuman4 <patelpradhyuman4444@gmail.com>
2024-11-07 18:34:28 +0530

    Add files via upload

---
+++ assg1_que3.java
@@ -0,0 +1,17 @@
+public class assg1_que3 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+if(a>b && a>c){
+    System.out.println("a is max " + a);
+}
+if(b>a && b>c){
+    System.out.println("b is max " + b);
+    
+}
+if (c>a && c>b){
+    System.out.println("c is max " + c);
+}
+    }
+}
